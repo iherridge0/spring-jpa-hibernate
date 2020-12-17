2e52dfa1599080444ef7eb4ca822dac0641d6451
Irwin Herridge <irwinherridge3@gmail.com>
2020-12-17 16:41:04 +0200

    OneToOne Mapping - Retrieving Student with Passport and Eager Fetch

--- src/test/java/za/co/iherridge0/jpa/hibernate/springjpahibernate/repository/StudentRepositoryTest.java
+++ src/test/java/za/co/iherridge0/jpa/hibernate/springjpahibernate/repository/StudentRepositoryTest.java
@@ -1,14 +1,16 @@
 package za.co.iherridge0.jpa.hibernate.springjpahibernate.repository;
 
+import javax.persistence.EntityManager;
+
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.transaction.annotation.Transactional;
 
 import za.co.iherridge0.jpa.hibernate.springjpahibernate.SpringJpaHibernateApplication;
+import za.co.iherridge0.jpa.hibernate.springjpahibernate.entity.Student;
 
 @SpringBootTest(classes = SpringJpaHibernateApplication.class)
 class StudentRepositoryTest {
@@ -18,11 +20,21 @@ class StudentRepositoryTest {
 	@Autowired
 	StudentRepository repository;
 
+	@Autowired
+	EntityManager em;
+
 	@Test
 	@DirtiesContext
-	@Transactional
 	void saveStudentWithPassport() {
 		repository.saveStudentWithPassport();
+	}
+
+	@Test
+	@DirtiesContext
+	void retrieveStudentAndPassport() {
+		Student student = em.find(Student.class, 20001L);
+		log.info("student -> {}", student);
+		log.info("passport -> {}", student.getPassport());
 	}
 
 }
