08e2d2488195c7e4621c2e0347c8499fc696ba5c
Irwin Herridge <irwinherridge3@gmail.com>
2020-12-17 10:37:07 +0200

    Create a JPA Project with H2 and Spring Boot

---
+++ src/test/java/za/co/iherridge0/jpa/hibernate/springjpahibernate/SpringJpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.iherridge0.jpa.hibernate.springjpahibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
