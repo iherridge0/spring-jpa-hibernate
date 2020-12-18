cc0a86c5aeb5d6c47e9d6144ba4bb91301718931
Irwin Herridge <irwinherridge3@gmail.com>
2020-12-18 15:33:26 +0200

    JPA Inheritance Hierarchies and Mappings - Single Table

--- src/main/java/za/co/iherridge0/jpa/hibernate/springjpahibernate/entity/Employee.java
+++ src/main/java/za/co/iherridge0/jpa/hibernate/springjpahibernate/entity/Employee.java
@@ -1,11 +1,16 @@
 package za.co.iherridge0.jpa.hibernate.springjpahibernate.entity;
 
 import javax.persistence.Column;
+import javax.persistence.DiscriminatorColumn;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
 
 @Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "EmployeeType")
 public abstract class Employee {
 
 	@Id
