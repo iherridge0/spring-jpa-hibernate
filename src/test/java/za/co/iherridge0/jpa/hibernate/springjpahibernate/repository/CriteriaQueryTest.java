095a659b8ac6839a4f407d9439bae5dd2a25ebc2
Irwin Herridge <irwinherridge3@gmail.com>
2020-12-21 11:15:15 +0200

    Criteria Query - Courses like 100 Steps

--- src/test/java/za/co/iherridge0/jpa/hibernate/springjpahibernate/repository/CriteriaQueryTest.java
+++ src/test/java/za/co/iherridge0/jpa/hibernate/springjpahibernate/repository/CriteriaQueryTest.java
@@ -6,6 +6,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
 import org.junit.jupiter.api.Test;
@@ -47,7 +48,7 @@ public class CriteriaQueryTest {
 		// 5. Build the TypedQuery using the entity manager and criteria query
 		TypedQuery<Course> query = em.createQuery(cq.select(courseRoot));
 		List<Course> resultList = query.getResultList();
-		log.info("Typed Query -> {}", resultList);
+		log.info("all_courses -> {}", resultList);
 
 		/*
 		 * OUTPUT Typed Query -> [Course [name=JPA in 50 Steps], Course [name=JPA in 86
@@ -56,8 +57,35 @@ public class CriteriaQueryTest {
 	}
 
 	@Test
-	public void test() {
+	public void all_courses_having_100Steps() {
+		// We want to do the following query:
+		// Select c From Course c
 
+		// 1. Use Criteria Builder to create a Criteria Query returning the expected
+		// result object
+
+		CriteriaBuilder cb = em.getCriteriaBuilder();
+		CriteriaQuery<Course> cq = cb.createQuery(Course.class);
+
+		// 2. Define roots for tables which are involved in the query
+
+		Root<Course> courseRoot = cq.from(Course.class);
+
+		// 3. Define Predicates etc using Criteria Builder
+		Predicate like100Steps = cb.like(courseRoot.get("name"), "%100 Steps");
+
+		// 4. Add Predicates etc to the Criteria Query
+		cq.where(like100Steps);
+
+		// 5. Build the TypedQuery using the entity manager and criteria query
+		TypedQuery<Course> query = em.createQuery(cq.select(courseRoot));
+		List<Course> resultList = query.getResultList();
+		log.info("all_courses_having_100Steps -> {}", resultList);
+
+		/*
+		 * OUTPUT Typed Query -> [Course [name=JPA in 50 Steps], Course [name=JPA in 86
+		 * Steps], Course [name=JPA in 100 Steps]]
+		 */
 	}
 
 }
